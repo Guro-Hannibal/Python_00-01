5225b98c16dfa5b7f0754a36a28a17cdc761b3e0
Guro <demensdeo@gmail.com>
2021-10-25 20:32:19 +0300

    уменьшил срез с 11 до 10

--- Lesson_2/lesson_2_hw_ex_4.py
+++ Lesson_2/lesson_2_hw_ex_4.py
@@ -3,6 +3,6 @@ list_string = string.split()
 print(list_string)
 for i in list_string:
     if len(i) > 10:
-        print(f'{list_string.index(i) + 1} {i[:11]}')
+        print(f'{list_string.index(i) + 1} {i[:10]}')
     else:
         print(f'{list_string.index(i)+1} {i}')
