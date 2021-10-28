fa69ccf547613a664b0d8bd2fb243fc6de8051f2
Guro <demensdeo@gmail.com>
2021-10-29 02:42:17 +0300

    demo

--- demo.py
+++ demo.py
@@ -1,5 +1,11 @@
 list_demo = [1, 2, 3, 4, 5, 6]
 
-new_list = {el: el**2 for el in list_demo if el % 2 == 0}
+expo_list = {el: el**2 for el in list_demo if el % 2 == 0}
 
-print(new_list)
+print(expo_list)
+
+list_demo_2 = ['wololo', 'wololo5', 'wololo', 'wololo5', 'wololo']
+
+list_wololo = {elem + ' expo ' + '-5': int(elem)**-5 for el in list_demo_2 for elem in el if elem == '5'}
+
+print(list_wololo)
