fade76e9f156007df0637ac68bd45b7864f0fc62
Guro <demensdeo@gmail.com>
2021-10-30 00:52:58 +0300

    Completed exercise 1

--- Lesson_4_hw_ex_1.py
+++ Lesson_4_hw_ex_1.py
@@ -1,9 +1,10 @@
 import sys
 
-money, a, b, c = sys.argv
+money, num1, num2, num3 = sys.argv
+
 
 def babosu(a, b, c):
     return a * b + c
 
 
-print(babosu(160, 800, 20000))
+print(babosu(int(num1), int(num2), int(num3)))
