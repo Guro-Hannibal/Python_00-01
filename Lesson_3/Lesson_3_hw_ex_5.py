a89c9660efc0f96e2e99b50138ed5f88654e940f
Guro <demensdeo@gmail.com>
2021-11-01 19:18:31 +0300

    Completed exercise 5

--- Lesson_3/Lesson_3_hw_ex_5.py
+++ Lesson_3/Lesson_3_hw_ex_5.py
@@ -4,17 +4,19 @@ foo_sum = 0
 def foo(a):
     list_demo = a.split()
     item_sum = 0
-    gogo = True
     global foo_sum
     if 'Q' in list_demo:
+        index_demo = list_demo.index('Q')
         list_demo.remove('Q')
-        gogo = False
+        for item in list_demo[:index_demo]:
+            item_sum += int(item)
+        foo_sum += item_sum
+        print(foo_sum)
+        return
     for item in list_demo:
         item_sum += int(item)
     foo_sum += item_sum
     print(foo_sum)
-    if not gogo:
-        return
     foo(input('Числа резделенные пробелом, для выхода введите \'Q\' : '))
 
 
