90b18fb96d398c01b0aa3e2ee02f03404d8fa2e0
Guro <demensdeo@gmail.com>
2021-11-14 06:18:24 +0300

    Completed exercise 3

--- Lesson_8/Lesson_8_hw_ex_3.py
+++ Lesson_8/Lesson_8_hw_ex_3.py
@@ -1,10 +1,10 @@
 demo_true = [12.69, '23', '43.420', 12, 65]
-demo_false = [14, 54, '15', 'abc', 432, 43]
+demo_false = [True, 14, 54, '15', 'abc', 432, 43,]
 
 
 class CheckList:
 
-    # проверка при инициализации в реальном времени
+    # проверка при инициализации
 
     def __init__(self):
         self.check_list = []
@@ -17,6 +17,16 @@ class CheckList:
             else:
                 print('Вы ввели не число')
 
+    @staticmethod
+    def is_num(check_num):
+        if isinstance(check_num, bool):
+            return False
+        try:
+            isinstance(float(check_num), float)
+            return True
+        except ValueError:
+            return False
+
     # проверка готового списка
 
     @staticmethod
@@ -26,16 +36,13 @@ class CheckList:
         except ValueError:
             return False
 
-    @staticmethod
-    def is_num(check_num):
-        try:
-            isinstance(float(check_num), float)
-            return True
-        except ValueError:
-            return False
-
 
 print(CheckList.is_num_list(demo_true))
 print(CheckList.is_num_list(demo_false))
 demo = CheckList()
 print(demo.check_list)
+print(CheckList.is_num(True))
+print(CheckList.is_num(False))
+print(float(True))
+print(float(False))
+print(isinstance('Anonymous', bool))
